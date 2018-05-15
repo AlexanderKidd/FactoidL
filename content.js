a9661448d8772024468653ef15e2970038182058
Alexander Kidd <alexanderkidd5@outlook.com>
2018-05-14 22:21:05 -0400

    Filtered-out general dateline and byline classes.

--- content.js
+++ content.js
@@ -1,17 +1,21 @@
 /*
  * @author Alexander Kidd
  * Created: 8/1/15
- * Revised: 5/13/18
+ * Revised: 5/14/18
  * Description: Script to be injected into
  * page for content scraping.
  *
  * WARNING: THIS IS LIVE SCRIPT THAT IS RUN TOP TO BOTTOM!
  */
 
+// Remove the dateline and byline at least, those don't need to be checked.
+var bylineSelector = ':not([class*="byline"])';
+var datelineSelector = ':not([class*="dateline"])';
+
 // Text scrapes based on HTML tags.
-var scrapedText = $('p').text();
+var scrapedText = $('p' + bylineSelector + datelineSelector).text();
 scrapedText.concat(' ');
-scrapedText.concat($('div').text());
+scrapedText.concat($('div' + bylineSelector + datelineSelector).text());
 scrapedText.concat(' ');
 scrapedText.concat($('caption').text());
 scrapedText.concat(' ');
@@ -25,17 +29,17 @@ scrapedText.concat($('address').text());
 scrapedText.concat(' ');
 scrapedText.concat($('cite').text());
 scrapedText.concat(' ');
-scrapedText.concat($('h1').text());
+scrapedText.concat($('h1' + bylineSelector + datelineSelector).text());
 scrapedText.concat(' ');
-scrapedText.concat($('h2').text());
+scrapedText.concat($('h2' + bylineSelector + datelineSelector).text());
 scrapedText.concat(' ');
-scrapedText.concat($('h3').text());
+scrapedText.concat($('h3' + bylineSelector + datelineSelector).text());
 scrapedText.concat(' ');
-scrapedText.concat($('h4').text());
+scrapedText.concat($('h4' + bylineSelector + datelineSelector).text());
 scrapedText.concat(' ');
-scrapedText.concat($('h5').text());
+scrapedText.concat($('h5' + bylineSelector + datelineSelector).text());
 scrapedText.concat(' ');
-scrapedText.concat($('h6').text());
+scrapedText.concat($('h6' + bylineSelector + datelineSelector).text());
 
 // The page title is usually brief enough for a related article search.
 var keyWords = $('title').text();
