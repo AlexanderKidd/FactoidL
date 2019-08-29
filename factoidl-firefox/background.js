4293b1d9c09743e46663ce005fc5fd8ee7f75a99
Alexander Kidd <alexanderkidd5@outlook.com>
2019-08-28 20:26:58 -0400

    Migrate to respective platform folders.

---
+++ factoidl-firefox/background.js
@@ -0,0 +1,275 @@
+/*
+ * @author Alexander Kidd
+ * Created: 8/1/15
+ * Revised: 8/12/19
+ * Description: Background page worker script.  Will
+ * handle the fact-checking tasks and pass it to the UI script (popup.js).
+ *
+ * This script should be registered in the extension manifest.
+ * HTML markup should not be required, it is meant for background
+ * JavaScript functions in most cases.
+ *
+ * Factoid: A statement (usually a full sentence) that may or may not be
+ * factual based on a common information source.
+ */
+
+var scrapedText; // Scraped text from the page to analyze.
+var pageKeyWords; // Used for Google search function on popup.html.
+var pageWideResults; // Text of source database query based on <title> keywords.
+var factoids; // scrapedText AFTER parsing into statements.
+var factRecord; // Keep track of which factoids were verified.
+var num = 0; // Numerator, factoids that are "accurate" (truthful).
+var den = 0; // Denominator, total factoids checked.
+var url = "-1"; // Store the url of the page being processed.
+var alreadyChecking = false; // Track whether this url is already checked or it is being checked.
+
+// Spin up workers to help with factoid comparison.
+var worker1 = new Worker('verifyWorker.js');
+var worker2 = new Worker('verifyWorker.js');
+var worker3 = new Worker('verifyWorker.js');
+
+// Regex escaper so a string can be passed into an expression object without fail.
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+/*
+ * General sentence-to-factoid parser.
+ * The ten-character limit was decided upon since most sentences under that are
+ * not complete sentences or do not have content worth fact-checking.  The max
+ * character limit of 2000 is generous: we are mostly checking modern articles,
+ * not Finnegan's Wake.  Also, strip out excessive whitespace.
+ *
+ * Returns the sanitized text (factoids).
+ */
+function sentenceParse() {
+  // Remove periods at the end of honorifics and in between acronyms (Note: won't catch all or sentences ending in abbreviations).
+  var nlpText = nlp(scrapedText.replace(/\.-/g, '. '));
+  var abbrList = nlpText.match('(#Acronym|#Abbreviation)').text();
+
+  abbrList.split(' ').forEach(function(token, index, arr) {
+    if(token) {
+      var re = new RegExp('\\s' + escapeRegExp(token) + '\\s', "g");
+      var replace = token.replace(/\./g, '');
+      scrapedText = scrapedText.replace(re, ' ' + replace + ' ');
+    }
+  });
+
+  return scrapedText.replace(/\n|\s{2,}/g, ' ').match(/[A-Z0-9][^.!?]{10,2000}[.!?\n]/g);
+}
+
+/*
+ * This function sifts through the factoid for keywords.
+ * This is used for querying the source database.
+ *
+ * Returns an array of string keywords, defaulting to just
+ * the factoid if both keyword extraction functions return empty.
+ */
+function getKeywords(factoid) {
+  var keyWords = nlp(factoid).topics().data().map(function(a) { return a.text.trim(); });
+
+  if(keyWords.length == 0) {
+    keyWords = socratesParser(factoid);
+
+    if(keyWords.length == 0) {
+      keyWords = factoid.split(" ");
+    }
+  }
+
+  return keyWords;
+}
+
+/*
+ * Iterates through factoids and calls queryForSources(),
+ * which kicks off the queries to find the proper source texts
+ * and then the factoid to source comparisons.
+ */
+function verifyFactoids(factoids) {
+  if(factoids !== null) {
+     // Find a default source text to look through.
+    queryForSources(pageKeyWords, -1, function(text) {
+      pageWideResults = text;
+
+      for(i = 0; i < factoids.length; i++) {
+        queryForSources(factoids[i], i, getSources);
+      }
+    });
+    // TODO: Coreference resolution: Replace ambiguous references with look-behind (e.g., pronouns in previous sentence).
+    // Maybe do IFF no terms found in sentence, go to previous sentence and pull.  How to determine that is tough...
+  }
+  else {
+    console.error("Error: verifyFactoids() had undefined or no factoids to check.");
+  }
+}
+
+/*
+ * Based on factoid keywords, finds candidate source text (Wikipedia articles)
+ * to use in factoid comparison.
+ */
+function queryForSources(factoid, index, callback) {
+  var factoidKeywords = getKeywords(factoid);
+  if(factoidKeywords == null || factoidKeywords.length == 0) factoidKeywords = getKeywords(pageKeyWords);
+
+  // Query seems to prefer one or two search terms.  Put two array elements together if there aren't two in the first element.
+  if(factoidKeywords[0].indexOf(" ") == -1 && factoidKeywords.length > 1 && factoidKeywords[1] && factoidKeywords[1] != factoidKeywords[0]) {
+    factoidKeywords = (factoidKeywords[0] + " " + factoidKeywords[1]).split(" ");
+  }
+  else {
+    factoidKeywords = factoidKeywords[0].split(" ").slice(0, 2);
+  }
+
+  $.ajax({
+    type: "GET",
+    url: "https://en.wikipedia.org/w/api.php?action=opensearch&pslimit=2&namespace=0&format=json&search=" +
+    encodeURIComponent(factoidKeywords),
+    dataType: "json",
+    async: true,
+    success: function (json) {
+      var sourceURL = json[3] ? json[3][0] : "";
+      callback.call(this, getSources(sourceURL, factoid, index));
+    },
+    error: function (xhr, status, error) {
+      factRecord[index] = "404";
+      den++; // Still increment that a factoid was processed even with failure.
+
+      console.error("Error: queryForSources() article search request errored for factoid {" + factoid + "}. Message: " + error +
+      "." + "\n" + "Site: " + url);
+    }
+  });
+}
+
+/*
+ * Loads the corresponding source text from the source URL for the factoid.
+ * Then kicks off a comparison job for one of the web workers.
+ */
+var getSources = function(sourceURL, factoid, index) {
+  $.ajax({
+    type: "GET",
+    url: sourceURL,
+    dataType: "text",
+    async: true,
+    success: function (text) {
+      if(index >= 0) {
+        if(index % 2 == 0 && index % 3 != 0) {
+          worker1.postMessage({ "factoid" : factoid, "index" : index, "text" : $('p, i', $.parseHTML(text.replace(/<img\b[^>]*>/ig, '').replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/igm, ''))).text(), "pageWideResults" : pageWideResults });
+        }
+        else if(index % 2 != 0 && index % 3 == 0) {
+          worker2.postMessage({ "factoid" : factoid, "index" : index, "text" : $('p, i', $.parseHTML(text.replace(/<img\b[^>]*>/ig, '').replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/igm, ''))).text(), "pageWideResults" : pageWideResults });
+        }
+        else {
+          worker3.postMessage({ "factoid" : factoid, "index" : index, "text" : $('p, i', $.parseHTML(text.replace(/<img\b[^>]*>/ig, '').replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/igm, ''))).text(), "pageWideResults" : pageWideResults });
+        }
+      }
+      else {
+        if(index == -1) {
+          pageWideResults = $('p, i', $.parseHTML(text.replace(/<img\b[^>]*>/ig, '').replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/igm, ''))).text();
+        }
+      }
+    },
+    error: function (xhr, status, error) {
+      console.error("Error: getSources() Wiki article request errored for factoid {" + factoid + "}. Message: " + error +
+      "." + "\n" + "Site: " + url);
+    }
+  });
+};
+
+/*
+ * The parser used for queries and matching factoids with reference text.
+ * Meant to be paired with socratesCompareStrategy() as the fact-checking algorithm.
+ * Initially, punctuation is removed and the factoid is split into a token (word) array.
+ * Then, unimportant words are replaced, keeping key (important nouns, verbs, adjectives) words as search terms.
+ *
+ * Returns an array of strings that are keywords (mostly content words).
+ */
+function socratesParser(factoid) {
+  // Remove punctuation.
+  var factoidParsed = factoid.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\]\[]/g, "").split(" ");
+
+  // Replace unimportant words for query/to search results:
+  for(k = 0; k < factoidParsed.length; k++) {
+    // Remove whitespace.
+    factoidParsed[k].trim();
+
+    // Article words:
+    factoidParsed[k] = factoidParsed[k].replace(/\b(a|an|the|this|that|these|those)\b/gi, "");
+
+    // Prepositions:
+    factoidParsed[k] = factoidParsed[k].replace(/\b(with|within|despite|beneath|through|throughout|until|upon|to|from|at|by|for|from|in|out|into|near|of|off|on|onto|up|down|with|over|under|past|since|between|above|below|across|after|before|during|except|front|back)\b/gi, "");
+
+    // Quantifiers:
+    factoidParsed[k] = factoidParsed[k].replace(/\b(some|many|few|much|all|enough|several|too|quite|rather)\b/gi, "");
+
+    // Pronouns:
+    factoidParsed[k] = factoidParsed[k].replace(/\b(I|we|us|you|she|her|him|it|he|they|them|my|mine|his|hers|your|yours|its|our|ours|their|theirs)\b/gi, "");
+
+    // Conjunctions:
+    factoidParsed[k] = factoidParsed[k].replace(/\b(and|or|nor|but|so|yet)\b/gi, "");
+
+    // Interrogatives:
+    factoidParsed[k] = factoidParsed[k].replace(/\b(who|whom|whose|which|what|how|why|when|where)\b/gi, "");
+
+    // Misc.:
+    factoidParsed[k] = factoidParsed[k].replace(/\b(has|was|is|yes|no|never|nobody|like|as|though|although)\b/gi, "");
+  }
+
+  // Remove replaced words.
+  factoidParsed = factoidParsed.filter(function (item) {
+    return (item !== "");
+  });
+
+  return factoidParsed;
+}
+
+/*
+ * Records and increments verified factoids and total factoids.
+ */
+var recordResults = function(returned_data, index) {
+  if(returned_data == 1) {
+    factRecord[index] = '1';
+    num++;
+  }
+
+  den++;
+};
+
+/*
+ * Receive compared fact results from workers.
+ */
+worker1.addEventListener('message', function(e) {
+  recordResults(e.data.isVerified, e.data.index);
+}, false);
+
+worker2.addEventListener('message', function(e) {
+  recordResults(e.data.isVerified, e.data.index);
+}, false);
+
+worker3.addEventListener('message', function(e) {
+  recordResults(e.data.isVerified, e.data.index);
+}, false);
+
+/*
+ * Listens for the content.js scrape of textual data.
+ */
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if(request.newCheck == true) {
+      url = request.url;
+      alreadyChecking = false;
+    }
+    else {
+      if(request.url == url && !alreadyChecking) {
+        num = 0;
+        den = 0;
+        scrapedText = request.data;
+        pageKeyWords = request.tags;
+        factoids = sentenceParse();
+        factRecord = factoids ? ['0'.repeat(factoids.length)] : [];
+        alreadyChecking = true;
+
+        if(factoids && factoids.length > 0) verifyFactoids(factoids);
+      }
+    }
+
+    // Mainly so Chrome doesn't complain the sender didn't receive a response.
+    sendResponse({result: true});
+});
---
+++ factoidl-firefox/popup.js
@@ -0,0 +1,298 @@
+/*
+ * @author Alexander Kidd
+ * Created: 8/1/15
+ * Revised: 8/14/19
+ * Description: Main UI and helper functions
+ * for fact-checking program pop-up.
+ *
+ * Verified Factoid: A statement that has been
+ * verified by the fact-checking algorithm/source used
+ * as true for all intents and purposes.
+ */
+
+var url;
+var parsedData;
+var factRecord;
+var totalFactoids;
+var keyWords;
+var factPct = -1;
+
+var pollInterval;
+var buildUIInterval;
+
+var isFactListVisible = false;
+
+var loadArcSize = degreesToRadians(45);
+var loadStartAngle = 0;
+var loadInc = 0;
+
+/*
+ * Pulls in the results once per popup window load from background.js script.
+ * Currently, calculates the percentage of "correct" (verified) factoids to total factoids.
+ */
+function pollFactData() {
+  var bg = chrome.extension.getBackgroundPage();
+
+  if(bg) {
+    url = bg.url;
+    parsedData = bg.factoids;
+    factRecord = bg.factRecord;
+    keyWords = bg.pageKeyWords;
+
+    // Default error if data could not be scraped or no data.
+    if(bg.scrapedText == "") {
+      clearInterval(buildUIInterval);
+      clearInterval(pollInterval);
+
+      $('#factoidl_icon').hide();
+      $('#check_button').hide();
+      $('#myCanvas').hide();
+      $('#fact_num').hide();
+      $('#links').hide();
+      $('#facts').hide();
+      document.getElementById("fact_text").style.color="#AD0000";
+      document.getElementById("fact_text").innerHTML = "No content found.<br><br>Try refreshing the page.";
+    }
+    else {
+      if(bg.factoids) {
+        if(bg.factoids.length == bg.den) {
+          totalFactoids = bg.den;
+          factPct = bg.num / bg.den;
+        }
+      }
+    }
+  }
+}
+
+/*
+ * Converts degrees to radians.
+ */
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+/*
+ * Responsible for composing main statistic as a pie chart:
+ * a percentage of verified factoids to total factoids checked.
+ */
+function drawPieChart() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var centerX = Math.floor(canvas.width / 2);
+  var centerY = Math.floor(canvas.height / 2);
+  var radius = Math.floor(canvas.width / 3);
+
+  // Filling verified percentage of pie chart starting at 12 o'clock.
+  var percentage = factPct;
+  var startingAngle = Math.PI * 1.5;
+  var arcSize = degreesToRadians(percentage * 360); // Multiply % by 360 degrees for proportion of circle.
+  if(arcSize <= 0) arcSize = 0.000001;
+  var endingAngle = startingAngle + arcSize;
+
+  ctx.lineWidth = 20;
+  ctx.strokeStyle = "#4ED25E";
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, startingAngle, endingAngle, false);
+  ctx.stroke();
+
+  // Complementing it, for the full doughnut chart effect.
+  ctx.lineWidth = 20;
+  ctx.strokeStyle = "#ECF0F1";
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius, endingAngle, startingAngle, false);
+  ctx.stroke();
+
+  // Add percentage readout.
+  if(percentage == 1.0) {
+    ctx.fillStyle = "#267F00";
+    ctx.font = "45px Arial";
+  }
+  if(percentage >= 0.8 && percentage < 1.0) {
+    ctx.fillStyle = "#267F00";
+    ctx.font = "50px Arial";
+  }
+  if(percentage < 0.8 && percentage > 0.6) {
+    ctx.fillStyle = "#FFFF00";
+    ctx.font = "50px Arial";
+  }
+  if(percentage <= 0.6 && percentage >= 0.1) {
+    ctx.fillStyle = "#E67E22";
+    ctx.font = "50px Arial";
+  }
+  if(percentage < 0.1) {
+    ctx.fillStyle = "#FF0000";
+    ctx.font = "50px Arial";
+  }
+
+  ctx.textAlign="center";
+
+  if(percentage == -1 || isNaN(percentage) || parsedData.length != totalFactoids) {
+    // Make pie chart a loading animation.
+    ctx.strokeStyle = "#4ED25E";
+    loadStartAngle = (1.5 * Math.PI * loadInc) / 100;
+    var loadEndAngle = loadStartAngle + loadArcSize;
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, loadStartAngle, loadEndAngle, false);
+    ctx.stroke();
+
+    // Complementing it, for the full doughnut chart effect.
+    ctx.strokeStyle = "#ECF0F1";
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, loadEndAngle, loadStartAngle, false);
+    ctx.stroke();
+
+    ctx.font = "bold 30px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Loading...", centerX, centerY + 15);
+
+    loadInc = (loadInc + 1) % (134);
+  }
+  else {
+    ctx.fillText(Math.round(percentage * 100) + "%", centerX, centerY + 15);
+    ctx.font = "bold 15px Arial";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Verified", centerX, centerY + 40);
+  }
+}
+
+/*
+ * This builds the main components of the popup UI, assuming valid data has been received
+ * from the background script (e.g., not null, not a different url than currently showing).
+ */
+function buildUI() {
+  var facts = document.getElementById('facts');
+
+  if(parsedData) setAnalysisUI(); // Transition from initial screen to factoid analysis UI.
+
+  drawPieChart();
+
+  // Generate a list of facts so the user knows what was checked and what to disregard.
+  facts.onclick = function() {
+    var list = document.createElement('ul');
+    $(list).css("list-style", "none");
+    $(list).css("padding", "0");
+    $(list).css("margin", "0");
+    list.id = "genList";
+
+    if(!isFactListVisible && parsedData && parsedData.length > 0) {
+      for(var i = 0; i < parsedData.length; i++) {
+        var factItem = document.createElement('li');
+        var factError = document.createElement('span');
+        var factBefore = "";
+        if(factRecord[i] == '1') {
+          factBefore = "✅";
+        }
+        else {
+          factBefore = "❌";
+        }
+
+        if(factRecord[i] == "404") {
+          factError.append(document.createTextNode("[404 Error - Could Not Check]"));
+          $(factError).css("color", "#AD0000");
+        }
+
+        factItem.append(document.createTextNode(factBefore + " "));
+        factItem.append(factError);
+        factItem.append(document.createTextNode(parsedData[i]));
+        $(factItem).css("padding", "5px 0px");
+        list.appendChild(factItem);
+      }
+
+      document.getElementById('factList').appendChild(list);
+      isFactListVisible = true;
+    }
+    else {
+      document.getElementById('factList').innerHTML = "";
+      isFactListVisible = false;
+    }
+
+    return false;
+  };
+
+  var linkSearch = document.getElementById('links');
+
+  if(keyWords) {
+    keyWords = keyWords.replace(/\s/g, "%20");
+    linkSearch.href = "https://www.google.com/search?q=" + keyWords;
+    $('#links').show();
+  }
+  else {
+    $('#links').hide();
+  }
+}
+
+/*
+ * Called to kick off scraping the page, polling the scraped text processing,
+ * and refreshing the UI.
+ */
+function startFactCheck() {
+  chrome.tabs.query({active:true, lastFocusedWindow:true}, function(tabArray) {
+    if(tabArray && tabArray.length > 0) {
+      chrome.runtime.sendMessage({newCheck: true, url: tabArray[0].url},
+        function (response) {
+           chrome.tabs.executeScript({file: "jquery-1.11.3.min.js"}, function() {
+             chrome.tabs.executeScript({file: "content.js"});
+           });
+        });
+    }
+  });
+
+
+  // Query the background script for factoid data.  This should probably be a listener of sorts.
+  pollFactData();
+  clearInterval(pollInterval);
+  pollInterval = setInterval(pollFactData, 500);
+
+  // Continuously build/update the UI as factoid data is processed.
+  buildUI();
+  clearInterval(buildUIInterval);
+  buildUIInterval = setInterval(buildUI, 250);
+}
+
+function setAnalysisUI() {
+  $('#factoidl_icon').hide();
+  $('#myCanvas').show();
+
+  document.getElementById("fact_text").style.color="#000000";
+  document.getElementById("fact_text").innerHTML = "Factoids checked at:" +
+  "<span id=\"current-link\" title=\"" + url + "\" style=\"display:block;width:200px;overflow:hidden;text-overflow:ellipsis;font-size:75%;\">" +
+  url + "</span>";
+
+  document.getElementById("fact_num").innerHTML = parsedData.length.toLocaleString();
+  document.getElementById("links").innerHTML = "<img border=\"0\" alt=\"Google Search\" src=\"search_icon_16x16.png\" width=\"16\" height=\"16\" style=\"vertical-align:-3px;\"> Related";
+  document.getElementById("facts").innerHTML = "Factoids <img border=\"0\" alt=\"Google Search\" src=\"fact_icon_16x16.png\" width=\"16\" height=\"16\" style=\"vertical-align:-3px;\">";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkButton = document.getElementById('check_button');
+    // onClick logic:
+    checkButton.addEventListener('click', function() {
+        startFactCheck();
+    });
+});
+
+/*
+ * Called when the popup window is loaded.
+ * Kicks off processing the current tab's page and building the UI.
+ */
+window.onload = function displayUI() {
+  pollFactData();
+
+  // Skip initial screen if already processing a page.
+  if(parsedData) {
+    if(parsedData.length > 0) {
+      setAnalysisUI();
+
+      // Query the background script for factoid data.  This should probably be a listener of sorts.
+      pollFactData();
+      clearInterval(pollInterval);
+      pollInterval = setInterval(pollFactData, 500);
+
+      // Continuously build/update the UI as factoid data is processed.
+      buildUI();
+      clearInterval(buildUIInterval);
+      buildUIInterval = setInterval(buildUI, 250);
+    }
+  }
+};
